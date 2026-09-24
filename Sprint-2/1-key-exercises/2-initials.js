a040e64fe665e8168f88a6f780b9173d5005a6f2
Sima <nongawuzasima@gmail.com>
2026-09-24 12:54:31 +0200

    Declared initials variable to take 1st letters

--- Sprint-2/1-key-exercises/2-initials.js
+++ Sprint-2/1-key-exercises/2-initials.js
@@ -2,9 +2,5 @@ const firstName = "Creola";
 const middleName = "Katherine";
 const lastName = "Johnson";
 
-// Declare a variable called initials that stores the first character of each string.
-// This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
-
-const initials = ``;
-
-// https://www.google.com/search?q=get+first+character+of+string+mdn
+const initials = `${firstName[0]}${middleName[0]}${lastName[0]}`;
+console.log(initials);
