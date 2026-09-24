fbabe0345d9a33566d72b75dcbd62214406bef9e
Sima <nongawuzasima@gmail.com>
2026-09-24 12:08:23 +0200

    Explanation of incrementing a variables

--- Sprint-2/1-key-exercises/1-count.js
+++ Sprint-2/1-key-exercises/1-count.js
@@ -4,3 +4,6 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+// ANSWER
+// In line 3, the value of 1 is added to the inital value of count which was 0, therefore the count value is being increasing by one each time the code runs and saves the new result into the count variable. The assignment operator "=" runs the everuthing on the right side and then store the result in the variable on the left hand side.
