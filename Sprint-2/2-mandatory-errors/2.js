2623dbd5543dceb94065ff2a2f10635067ffe70a
Sima <nongawuzasima@gmail.com>
2026-09-24 15:29:04 +0200

    Make the cityOfBirth the first line before the console.log

--- Sprint-2/2-mandatory-errors/2.js
+++ Sprint-2/2-mandatory-errors/2.js
@@ -1,5 +1,8 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
 
-console.log(`I was born in ${cityOfBirth}`);
+//Answer
+// The console.log will be execued first because it is before the declared city variable.
+
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
